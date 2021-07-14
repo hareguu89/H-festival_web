f2236a259b8c72092ac08d4e4ddf132be9480a22
Jung Hwan Park <69840717+hareguu89@users.noreply.github.com>
2021-07-14 16:16:39 +0900

    Update Dealers.js

--- server/controller/Dealers.js
+++ server/controller/Dealers.js
@@ -1,4 +1,4 @@
-const { dealer, image } = require("../models");
+const { dealer, image, file } = require("../models");
 const sequelize = require("sequelize");
 const { Sequelize } = require("sequelize");
 const Op = sequelize.Op;
@@ -13,17 +13,16 @@ module.exports = {
     let user = await dealer.create({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
+      mobile: req.body.mobile,
       email: req.body.email,
       region: req.body.region,
       country: req.body.country,
       sex: req.body.sex,
-      mediaDest: req.body.mediaDest,
       selectedDest: req.body.selectedDest,
       description: req.body.description,
     });
 
     let imgSrc = req.body.picture.src;
-
     for (let i = 0; i < imgSrc.length; i++) {
       await image.create({
         dealerId: user.dataValues.id,
@@ -31,7 +30,15 @@ module.exports = {
       });
     }
 
-    if (user && imageTable) {
+    let files = req.body.mediaDest.src;
+    for (let i = 0; i < files.length; i++) {
+      await file.create({
+        dealerId: user.dataValues.id,
+        file: files[i],
+      });
+    }
+
+    if (user) {
       res.status(200).json("완료.");
     } else {
       res.status(404).json("error");
